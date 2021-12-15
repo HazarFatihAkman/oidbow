d25d509a42d93385e7a087c785e7d0a449100859
Hazar Fatih Akman <74676200+HazarFatihAkman@users.noreply.github.com>
2021-12-15 08:29:59 +0300

    v1.1 - methods name etc. changed - methods logic changed - getting url error about image url create from DownloadImg method ctrl-g : 70

--- func.py
+++ func.py
@@ -8,8 +8,8 @@ import re
 
 
 class Func():
-    def replaceAll(self,repstring):
-        replacedict = {
+    def ReplaceAll(self,repString):
+        replaceDict = {
         'ı':'i',
         'ğ':'g',
         'ü':'u',
@@ -26,117 +26,117 @@ class Func():
         '*':'-',
         '.':'-',
         ',':'-',
-        '/':'-',
-        'ÄŸ':'g',
-        'Ä±':'i',
-        'ÅŸ':'s',
-        'Ãœ':'U',
-        'Ã¼':'u',
-        'Ã¶':'o',
-        'Ã§':'c',
-        'Ä°':'i',
-        'Ã‡':'C',
-        'Ã–':'O'
+        '/':'-'
         }
         newStr = ''
-        for c in repstring:
-            if replacedict.get(c) != None:
-                newStr = newStr + replacedict[c]
+        for c in repString:
+            if replaceDict.get(c) != None:
+                newStr = newStr + replaceDict[c]
             else:
                 newStr = newStr + c
         return newStr
 #-    
-    def foldercontcre(self,foldername):
-        if os.path.exists(foldername) == False:
-            os.mkdir(foldername)
+    def FolderControlCreate(self,folderName):
+        if os.path.exists(folderName) == False:
+            os.mkdir(folderName)
             print('Folder creating. ')    
         else:
             print('Folder created. ')       
 #-  
-    def searchlink(self,source,hrefid,urlsite):
-        atagarry = []              
-        for atag in tqdm(source.find_all("a"), "Products link are being scanned"):
-            atags = urlsite+atag.attrs.get("href")
-            atagfind = str(atags).find(hrefid)                            
-            if not atags:
+    def SearchLink(self,source,hrefId,url):
+        aTagArray = []              
+        for aTag in tqdm(source.find_all("a"), "Products link are being scanned"):
+            aTags = aTag.attrs.get("href")
+            aTagFind = str(aTags).find(hrefId)
+            
+            if not aTags:
                 continue
             else:
-                if atagfind != -1:
-                    if len(atagarry) == 0 or atagarry[len(atagarry)-1] != atags:
-                        atagarry.append(atags)
-        return atagarry        
+                if aTagFind != -1:
+                        aTagArray.append(aTags)
+        return aTagArray        
 #-  
-    def downloadimg(self,ahref,findta,folders,urlsite,inputun):
-            r = requests.get(ahref)
-            source = BeautifulSoup(r.content,"html.parser")           
+    def DownloadImg(self,aHref,findData,folders,url,inputValue):
+            r = requests.get(aHref)
+            source = BeautifulSoup(r.content,"html.parser")
+            imgTypes = {
+                'jpg' : 'jpg',
+                'png' : 'png',
+                'jpeg' : 'jpeg',
+                'webp' : 'webp'
+            }
             for img in tqdm(source.find_all("img"), "Pictures are being scanned"):                   
-                img_url = urlsite+"/"+img.attrs.get("src")
-                imgnamesp = img.attrs.get("src").split('/')
-                img_ex = img.attrs.get("src").split('.')
-                img_ex = img_ex[len(img_ex)-1]    
-                if not img_url:
+                imgUrl = img.attrs.get("src")
+                if(str(imgUrl).find('http') == -1 | str(imgUrl).find('https') == -1):
+                    imgUrl = url+imgUrl
+                imgNameSplit = img.attrs.get("src").split('/')
+                imgType = img.attrs.get("src").split('.')
+                print(imgUrl)
+                if not imgUrl:
                   continue
                 else:
-                  imgfind = str(img_url).find(findta)
-                  if(imgfind != -1 ):
-                    if(img_ex == 'jpg' or img_ex == 'png' or img_ex == 'jpeg' or img_ex == 'webp'):                         
-                        imgname = folders+"/"+imgnamesp[len(imgnamesp)-1]
-                        urllib.request.urlretrieve(img_url,imgname)
-                        if inputun.upper() == 'Y':
-                            image = Image.open(imgname)
-                            w, h = image.size
-                            if int(w) >= 1100 or int(h) >= 1100:
-                                w = w/4
-                                h = h/4
-                            else:
-                                wif = w/2
-                                hif = h/2 
-                        
-                                if wif >= 225 or hif >= 225:
-                                    w = w/2
-                                    h = h/2
-                                else:
-                                    w = w/3
-                                    h = h/3
-                            if w >= 225 or h >= 225:
-                                new_image = image.resize((int(w), int(h))).convert("RGB")
-                                new_image.save(imgname)
-                    else:
-                        continue
+                  if(imgTypes.get(imgType[1]) and str(imgUrl).find(findData) != -1):                                              
+                      imgName = folders+"/"+imgNameSplit[len(imgNameSplit)-1]
+                      opener = urllib.request.URLopener()
+                      opener.addheader('User-Agent', 'whatever')
+                      opener.retrieve(imgUrl, imgName)
+                      if inputValue.upper() == 'Y':
+                          image = Image.open(imgName)
+                          w, h = image.size
+                          if int(w) >= 1100 or int(h) >= 1100:
+                              w = w/4
+                              h = h/4
+                          else:
+                              wif = w/2
+                              hif = h/2 
+                      
+                              if wif >= 225 or hif >= 225:
+                                  w = w/2
+                                  h = h/2
+                              else:
+                                  w = w/3
+                                  h = h/3
+                          if w >= 225 or h >= 225:
+                              new_image = image.resize((int(w), int(h))).convert("RGB")
+                              new_image.save(imgName)
+                  else:
+                      continue              
 #-  
-    def datamining(self,href,tags,stock,profolder):
+    def DataMining(self,href,tags):
         repdict = {
             '<':'',
+            "'":'',
             '"':'',
             '=':' ',
             '>':''
         }
-        retunarray = []
-        protags = tags.split(',')
-        protags.append(stock)
+        retunArray = []
+        productTags = tags.split(',')
         r = requests.get(href)
         source = BeautifulSoup(r.content,"html.parser")
-        for ptag in protags:
+        for productTag in productTags:
             newStr = ''
-            for ptg in ptag:
+            for ptg in productTag:
                 if repdict.get(ptg) != None:
                     newStr = newStr + repdict[ptg]
                 else:
                     newStr = newStr + ptg
+                    
             newStr = newStr.split(' ')
+            
             finds = source.find_all(newStr[0],attrs={newStr[1]:newStr[2]})
-            finds = re.sub(r'^.*">', "",str(finds[0])).replace("</"+newStr[0]+">",'')
-            retunarray.append(finds)
-        return retunarray
+            if(len(finds) > 0):
+                retunArray.append(finds[0])
+        return retunArray
 #-  
-    def creathtmlInfo(self,Infarray,folder):
+    def CreateHtmlInfo(self,title,infoArray,folder):
         html_cd = folder+"/index.html"
         if os.path.exists(html_cd) == False:
-            readf = open(html_cd,"a", encoding='utf8')
-            html_head = '<html><head><meta charset="utf-8" /><meta http-equiv="x-ua-compatible" content="ie=edge" /><meta name="viewport" content="width=device-width, initial-scale=1" /><title>'+Infarray[0]+'</title></head><body>'
-            writ = html_head+''.join([str(elem) for elem in Infarray])+'</body></html>'
-            datam = readf.write(writ)
-            readf.close()
+            readFolder = open(html_cd,"a", encoding='utf8')
+            html_head = '<html><head><meta charset="utf-8" /><meta http-equiv="x-ua-compatible" content="ie=edge" /><meta name="viewport" content="width=device-width, initial-scale=1" /><title>'+title+'</title></head><body>'
+            writeFolder = html_head+''.join([str(elem) for elem in infoArray])+'</body></html>'
+            readFolder.write(writeFolder)
+            readFolder.close()
             return print("Index creating.")
         else:                
             return print("Index created.")
--- index.py
+++ index.py
@@ -4,8 +4,15 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import numpy as np
-
 import func
+import json
+import locale
+
+with open("config.json", encoding='utf-8') as jsonFile:
+    jsonObject = json.load(jsonFile)
+    jsonFile.close()
+
+locale.setlocale(locale.LC_ALL, jsonObject['localeTimeZone'])
 
 os.system("color b")
 os.system("cls")
@@ -18,54 +25,44 @@ print("#"*15+"-"*30+"#"*15)
 print(" "*20+"Program Starting up"+" "*20)
 print("#"*15+"-"*30+"#"*15)
 
-readf = open("index.txt","r", encoding='utf8')
-datam = readf.read().split('\n')
-readf.close()
-
 time.sleep(1)
-if(len(datam) > 0):
-    onurl = datam[0]
-    link = datam[1]
-    mainfol = datam[2]
-    mainaddfol = datam[3]
-    ahrefid = datam[4]
-    protag = str(datam[5])
-    stok = datam[6]
-    imgid = datam[7]
+if(len(jsonObject) > 0):
+    
+    productsUrl = jsonObject['mainUrl'] + jsonObject['subPath']
 
-    funct = func.Func()
-
-    funct.foldercontcre("Folders")
-    maindict = "Folders/"+funct.replaceAll(mainfol)
-    funct.foldercontcre(maindict)
-    mainadict = maindict+"/"+funct.replaceAll(mainaddfol)
-    funct.foldercontcre(mainadict)
+    functionClass = func.Func()
+    functionClass.FolderControlCreate(jsonObject['folderPath'])
+    folderMainPath = jsonObject['folderPath']+"/"+functionClass.ReplaceAll(str(jsonObject['mainFolderName']).lower())
+    functionClass.FolderControlCreate(folderMainPath)
+    
+    subDict = folderMainPath+"/"+functionClass.ReplaceAll(jsonObject['subFolderName'])
+    functionClass.FolderControlCreate(subDict)
     #creat base folders
-
-    r = requests.get(link)
+    r = requests.get(productsUrl)
     source = BeautifulSoup(r.content,"html.parser")
     #products link searching    
     print("#"*15+"-"*30+"#"*15)
     print(" "*17+"Info is gathering & saving"+" "*17)
     print("#"*15+"-"*30+"#"*15)
-    aharray = funct.searchlink(source,ahrefid,onurl)   
-    print("Number of products : "+str(len(aharray)))
-
+    aTagArray = functionClass.SearchLink(source,jsonObject['aHrefId'],jsonObject['mainUrl'])
+    print("Number of products : "+str(len(aTagArray)))
     print(r"Do yout want to image auto resizing?(Y\N)")
-    user_in = input()
-    end_nmbr = 1
-    for ahref in aharray:
-        data_turn = funct.datamining(ahref,protag,stok,mainadict)
-        title_link = funct.replaceAll(data_turn[0])
-        pro_folder = mainadict+"/"+title_link
-        print(str(end_nmbr)+"/"+str(len(aharray))+" : "+data_turn[0])
-        funct.foldercontcre(pro_folder)
-        funct.creathtmlInfo(data_turn,pro_folder)
-        funct.downloadimg(ahref,imgid,pro_folder,onurl,user_in)
-        end_nmbr = end_nmbr + 1  
-        print("-"*60)
-
-    print("Process completed. Edit the link and subcategory of the other page in index.txt.")        
+    userInput = input()
+    endNbr = 1
+    
+    for ahref in aTagArray:
+       newUrl = jsonObject['mainUrl']+ahref 
+       dataResponse = functionClass.DataMining(newUrl,str(jsonObject['infoTags']))
+       title = ahref.replace(jsonObject['aHrefId'], '') 
+       titleUrl = functionClass.ReplaceAll(title)
+       productFolder = subDict+"/"+titleUrl
+       print(str(endNbr)+"/"+str(len(aTagArray))+" : "+ title)
+       functionClass.FolderControlCreate(productFolder)
+       functionClass.CreateHtmlInfo(title,dataResponse,productFolder)
+       functionClass.DownloadImg(newUrl,jsonObject['imgGeneralPath'],productFolder,jsonObject['mainUrl'],userInput)
+       endNbr = endNbr + 1  
+       print("-"*60)
+    print("Process completed. Edit the link and subcategory of the other page in config.json.")        
 else:
     print("Fill in the index file.")
     quit
